4f8b3aed6a3b3f294c2a9601e7334257879313cd
Chamod Priyamal <chammaofficial@gmail.com>
2026-03-01 21:58:20 +0800

    Add fundamentals of programming up to week 2

---
+++ Y2/S1/Fundamentals-of-Programming-COMP5005/FOP/Prac01/Monty/Pythons/Flying/bruces.py
@@ -0,0 +1,13 @@
+#
+# bruces.py - let's call everyone "Bruce", to avoid confusing.
+#
+
+name = input("\nHey cobber, what's your name? ")
+if name != "Bruce":
+    print("Sorry,",name," - your name's not Bruce?")
+    print("That's going to cause a little confusion.")
+    print("Mind if we call you 'Bruce' to keep it clear")
+    name = "Bruce"
+else:
+    print("Excellent! That saves a lot of confusion")
+print("G'day", name, "!!!\n")
---
+++ Y2/S1/Fundamentals-of-Programming-COMP5005/FOP/Prac01/The/Holy/Grail/bridge2.py
@@ -0,0 +1,6 @@
+print("\nWelcome to the Bridge of Death\n")
+name = input("What is your name?")
+quest = input("What is your quest?")
+colour = input("What is your favourite colour?")
+print("\nHello,", name, "good luck with your", quest, "quest!")
+print("Perhaps wearing", colour, "socks would help :)\n")
