0cbd776768231fdc5102c474eb040111f2d9e033
Chamod Priyamal <chammaofficial@gmail.com>
2026-03-13 20:41:16 +0800

    Week 2 Lab is Done

--- Y2/S1/Web-Application-Framework-COMP6006/Week 02/blogproject/blogapp/views.py
+++ Y2/S1/Web-Application-Framework-COMP6006/Week 02/blogproject/blogapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blogapp.models import Post
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    postsQuerySet = Post.objects.all()
+    return render(request,'index.html',{'posts':postsQuerySet})
+
